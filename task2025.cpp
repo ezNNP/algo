25a0fbb6402de998e9eb39fc4d03c49e2009a7af
grigoriy <grisha.56@icloud.com>
2020-02-10 10:42:49 +0300

    task 2025 from acm.timus

---
+++ task2025.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve_and_print(int n, int k);
+
+int main(int argc, const char * argv[]) {
+    int T;
+    cin >> T; // <-- количество тестов
+
+    for (int i = 0; i < T; ++i) {
+        int n, k;
+        cin >> n >> k;
+        solve_and_print(n, k);
+    }
+}
+
+void solve_and_print(int n, int k) {
+    int nn;
+    if (n % k == 0) {
+        nn = n;
+    } else {
+        nn = n + k - (n % k);
+    }
+    int* teams = new int[10000];
+    for (int i = 0; i < k; ++i) {
+        teams[i] = nn / k;
+    }
+    int dec_team_pointer = 0;
+    while (nn > n) {
+        teams[dec_team_pointer++]--;
+        nn--;
+        if (dec_team_pointer == k) {
+            dec_team_pointer = 0;
+        }
+    }
+    long long result = 0;
+    for (int i = 0; i < k - 1; ++i) {
+        for (int j = i+1; j < k; ++j) {
+            result += teams[i] * teams[j];
+        }
+    }
+
+    delete [] teams;
+
+    cout << result << endl;
+}
+
