c87e2667ebcc953f36c80b7d9e875f4e1501e1e0
grigoriy <grisha.56@icloud.com>
2020-02-14 17:10:43 +0300

    1494

---
+++ third/1494.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(int  argc, const char * argv[]) {
+    int n;
+
+    cin >> n;
+
+    stack<int> arr;
+
+    bool cheater = false;
+
+    int last, current;
+
+    last = 0;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> current;
+        for (int j = last+1; j <= current; ++j) {
+            arr.push(j);
+        }
+        last = last > current ? last : current;
+        if (arr.top() == current) {
+            arr.pop();
+        } else {
+            cheater = true;
+            break;
+        }
+    }
+
+    if (cheater) {
+        cout << "Cheater";
+    } else {
+        cout << "Not a proof";
+    }
+
+    return 0;
+}
